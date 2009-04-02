4b27209ccde6bd4209d42974eaf3f8e2efd7d52e
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2009-04-01 18:15:12 -0700

    first checkin of 15minutes

---
+++ 15minutes/FormMain.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace _15minutes
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.buttonStartOk = new System.Windows.Forms.Button();
+            this.label5min = new System.Windows.Forms.Label();
+            this.label15min = new System.Windows.Forms.Label();
+            this.label30min = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.buttonPauseResume = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonStartOk
+            // 
+            this.buttonStartOk.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.buttonStartOk.Location = new System.Drawing.Point(0, 100);
+            this.buttonStartOk.Name = "buttonStartOk";
+            this.buttonStartOk.Size = new System.Drawing.Size(290, 23);
+            this.buttonStartOk.TabIndex = 0;
+            this.buttonStartOk.Text = "Start countdown";
+            this.buttonStartOk.UseVisualStyleBackColor = true;
+            this.buttonStartOk.Click += new System.EventHandler(this.buttonStartOk_Click);
+            // 
+            // label5min
+            // 
+            this.label5min.AutoSize = true;
+            this.label5min.Location = new System.Drawing.Point(252, 9);
+            this.label5min.Name = "label5min";
+            this.label5min.Size = new System.Drawing.Size(32, 13);
+            this.label5min.TabIndex = 1;
+            this.label5min.Text = "5 min";
+            this.label5min.Click += new System.EventHandler(this.label5min_Click);
+            // 
+            // label15min
+            // 
+            this.label15min.AutoSize = true;
+            this.label15min.Location = new System.Drawing.Point(246, 21);
+            this.label15min.Name = "label15min";
+            this.label15min.Size = new System.Drawing.Size(38, 13);
+            this.label15min.TabIndex = 2;
+            this.label15min.Text = "15 min";
+            this.label15min.Click += new System.EventHandler(this.label15min_Click);
+            // 
+            // label30min
+            // 
+            this.label30min.AutoSize = true;
+            this.label30min.BackColor = System.Drawing.SystemColors.Control;
+            this.label30min.Location = new System.Drawing.Point(246, 34);
+            this.label30min.Name = "label30min";
+            this.label30min.Size = new System.Drawing.Size(38, 13);
+            this.label30min.TabIndex = 3;
+            this.label30min.Text = "30 min";
+            this.label30min.Click += new System.EventHandler(this.label30min_Click);
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Location = new System.Drawing.Point(152, 100);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(139, 23);
+            this.buttonStop.TabIndex = 4;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Visible = false;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // buttonPauseResume
+            // 
+            this.buttonPauseResume.Location = new System.Drawing.Point(0, 100);
+            this.buttonPauseResume.Name = "buttonPauseResume";
+            this.buttonPauseResume.Size = new System.Drawing.Size(152, 23);
+            this.buttonPauseResume.TabIndex = 5;
+            this.buttonPauseResume.Text = "Pause";
+            this.buttonPauseResume.UseVisualStyleBackColor = true;
+            this.buttonPauseResume.Visible = false;
+            this.buttonPauseResume.Click += new System.EventHandler(this.buttonPauseResume_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(290, 123);
+            this.Controls.Add(this.buttonPauseResume);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.label30min);
+            this.Controls.Add(this.label15min);
+            this.Controls.Add(this.label5min);
+            this.Controls.Add(this.buttonStartOk);
+            this.MaximumSize = new System.Drawing.Size(298, 152);
+            this.MinimumSize = new System.Drawing.Size(298, 152);
+            this.Name = "FormMain";
+            this.Text = "15minutes";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStartOk;
+        private System.Windows.Forms.Label label5min;
+        private System.Windows.Forms.Label label15min;
+        private System.Windows.Forms.Label label30min;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.Button buttonPauseResume;
+    }
+}
+
---
+++ 15minutes/FormMain.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Text;
+using System.Windows.Forms;
+
+namespace _15minutes
+{
+    public enum State
+    {
+        SettingTime, Running, Paused, Finished
+    }
+
+    public partial class FormMain : Form
+    {
+        int Hours;
+        int Minutes;
+        int Seconds;
+
+        TimeSpan TotalTime;
+        TimeSpan RemainingTime;
+        State CurrentState;
+        DateTime StartTime;
+
+        public void SetTime(int hours, int minutes, int seconds)
+        {
+            Hours = hours;
+            Minutes = minutes;
+            Seconds = seconds;
+            TotalTime = new TimeSpan(hours, minutes, seconds);
+            this.Invalidate();
+        }
+
+        public FormMain()
+        {
+            InitializeComponent();
+            this.label5min.MouseEnter += new EventHandler(label_MouseEnter);
+            this.label5min.MouseLeave += new EventHandler(label_MouseLeave);
+            this.label15min.MouseEnter += new EventHandler(label_MouseEnter);
+            this.label15min.MouseLeave += new EventHandler(label_MouseLeave);
+            this.label30min.MouseEnter += new EventHandler(label_MouseEnter);
+            this.label30min.MouseLeave += new EventHandler(label_MouseLeave);
+            this.DoubleBuffered = true;
+            SwitchToSettingTimeState();
+        }
+
+        public void SwitchToSettingTimeState()
+        {
+            CurrentState = State.SettingTime;
+
+            // TODO: remember what it was last
+            SetTime(0, 15, 0);
+            //SetTime(0, 0, 5);
+            timer.Stop();
+
+            label5min.Visible = true;
+            label15min.Visible = true;
+            label30min.Visible = true;
+
+            buttonStartOk.Text = "Start countdown";
+            buttonStartOk.Visible = true;
+
+            buttonPauseResume.Visible = false;
+            buttonStop.Visible = false;
+
+            this.BackColor = SystemColors.Control;
+            this.Invalidate();
+        }
+
+        public void SwitchToRunningState(bool resetTime)
+        {
+            CurrentState = State.Running;
+
+            label5min.Visible = false;
+            label15min.Visible = false;
+            label30min.Visible = false;
+
+            buttonStartOk.Visible = false;
+
+            buttonPauseResume.Visible = true;
+            buttonStop.Visible = true;
+
+            if (resetTime)
+                StartTime = DateTime.Now;
+            CalcRemaining();
+            timer.Interval = 1000;
+            timer.Start();
+            this.Invalidate();
+        }
+
+        public void SwitchToPausedState()
+        {
+            CurrentState = State.Paused;
+
+            label5min.Visible = false;
+            label15min.Visible = false;
+            label30min.Visible = false;
+
+            buttonStartOk.Visible = false;
+
+            buttonPauseResume.Visible = true;
+            buttonStop.Visible = true;
+
+            timer.Stop();
+            buttonPauseResume.Text = "Resume";
+            this.Invalidate();
+        }
+
+        public void SwitchToFinishedState()
+        {
+            CurrentState = State.Finished;
+
+            label5min.Visible = false;
+            label15min.Visible = false;
+            label30min.Visible = false;
+
+            buttonStartOk.Visible = true;
+            buttonStartOk.Text = "Ok";
+
+            buttonPauseResume.Visible = false;
+            buttonStop.Visible = false;
+
+            timer.Stop();
+            timer.Interval = 200;
+            timer.Start();
+
+            this.Invalidate();
+        }
+
+        private void label_MouseEnter(object sender, EventArgs e)
+        {
+            Control c = sender as Control;
+            c.BackColor = System.Drawing.Color.White;
+        }
+
+        private void label_MouseLeave(object sender, EventArgs e)
+        {
+            Control c = sender as Control;
+            c.BackColor = System.Drawing.SystemColors.Control;
+        }
+
+        private void label5min_Click(object sender, EventArgs e)
+        {
+            SetTime(0, 5, 0);
+        }
+
+        private void label15min_Click(object sender, EventArgs e)
+        {
+            SetTime(0, 15, 0);
+        }
+
+        private void label30min_Click(object sender, EventArgs e)
+        {
+            SetTime(0, 30, 0);
+        }
+
+        private void buttonStartOk_Click(object sender, EventArgs e)
+        {
+            if (CurrentState == State.SettingTime)
+            {
+                SwitchToRunningState(true);
+            }
+            else
+            {
+                Debug.Assert(CurrentState == State.Finished);
+                SwitchToSettingTimeState();
+            }
+        }
+
+        private bool CalcRemaining()
+        {
+            DateTime curr = DateTime.Now;
+            TimeSpan passed = curr - StartTime;
+            if (passed > TotalTime)
+                RemainingTime = new TimeSpan(0, 0, 0);
+            else
+                RemainingTime = TotalTime - passed;
+            return passed > TotalTime;
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if (CurrentState == State.Running)
+            {
+                bool shouldStop = CalcRemaining();
+                if (shouldStop)
+                    SwitchToFinishedState();
+            }
+            else
+            {
+                Debug.Assert(CurrentState == State.Finished);
+                if (this.BackColor == Color.White)
+                {
+                    this.BackColor = Color.Red;
+                }
+                else
+                {
+                    this.BackColor = Color.White;
+                }
+            }
+            this.Invalidate(true);
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            TimeSpan timeToShow = RemainingTime;
+            if (CurrentState == State.SettingTime)
+                timeToShow = TotalTime;
+            int hours = timeToShow.Hours;
+            int minutes = timeToShow.Minutes;
+            int seconds = timeToShow.Seconds;
+
+            using (var font = new Font("Arial", 24, FontStyle.Bold))
+            {
+                Graphics g = e.Graphics;
+                g.SmoothingMode = SmoothingMode.AntiAlias;
+                Rectangle rect = this.ClientRectangle;
+                int dx = rect.Width;
+                int dy = rect.Height - buttonStartOk.Height;
+                string s = String.Format("{0:D2} : {1:D2} : {2:D2}", hours, minutes, seconds);
+                SizeF strSize = g.MeasureString(s, font);
+
+                float x = (dx - strSize.Width) / 2;
+                float y = (dy - strSize.Height) / 2;
+                PointF pos = new PointF(x, y);
+                g.DrawString(s, font, Brushes.Black, pos);
+            }
+            base.OnPaint(e);
+        }
+
+        private void buttonPauseResume_Click(object sender, EventArgs e)
+        {
+            if (CurrentState == State.Running)
+                SwitchToPausedState();
+            else 
+            {
+                Debug.Assert(CurrentState == State.Paused);
+                SwitchToRunningState(false);
+            }
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            SwitchToSettingTimeState();
+        }
+    }
+}
